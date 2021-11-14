53e88a056d74c89ec7e085c2e02e195b73d7ae2b
codeit <080821@naver.com>
2021-11-14 18:17:00 +0900

    Merge branch 'test' into prem

---
+++ cal.py
@@ -0,0 +1,17 @@
+def add(a,b):
+return a+b
+
+def subtract(a,b):
+return a-b
+
+def multiply(a,b):
+	return a*b
+
+def divide_test(a,b):
+	return a/b
+
+def say(a):
+	return a+"안녕"
+
+def get_Median(a,b):
+	return (a*b)/2
